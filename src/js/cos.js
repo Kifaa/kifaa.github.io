f49cc6117b4d43e8778bd24a65bd71b6877b640b
Kifaa <hakifkadriu2003@gmail.com>
2023-10-17 17:19:59 +0200

    added a calculation program

---
+++ src/js/cos.js
@@ -0,0 +1,5 @@
+function calculate(){
+    d = document.getElementById("d").value;
+    a = document.getElementById("a").value;
+    document.getElementById("result").innerHTML = "The result is: " + d * a / 100 + " litres";
+}
